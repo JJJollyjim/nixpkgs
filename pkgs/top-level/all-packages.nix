10c8ae181a07dd9d83c91ed5e5add5b3b8780d36
Jamie McClymont <jamie@kwiius.com>
2020-05-14 13:41:45 +1200

    WIP linux 5.6 woo

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17019,7 +17019,7 @@ in
   });
 
   # The current default kernel / kernel modules.
-  linuxPackages = linuxPackages_5_4;
+  linuxPackages = linuxPackages_5_6;
   linux = linuxPackages.kernel;
 
   # Update this when adding the newest kernel major version!
